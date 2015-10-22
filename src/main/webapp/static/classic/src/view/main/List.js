5d081ac3d6a1b995027dd493ceb48f7c4d620079
Dou's MacBook <cruiserdou@qq.com>
2015-10-22 11:15:43 +0800

    Add index.html loading info.

--- src/main/webapp/static/classic/src/view/main/List.js
+++ src/main/webapp/static/classic/src/view/main/List.js
@@ -15,8 +15,8 @@ Ext.define('app.view.main.List', {
                     { text: 'detention', leaf: true },
                     { text: 'homework', expanded: true, children: [
                         { text: 'book report', leaf: true },
-                        { text: 'algebra', leaf: true}
-                    ] },
+                        { text: 'algebra', leaf: true}]
+                    },
                     { text: 'buy lottery tickets', leaf: true }
                 ]
             }
