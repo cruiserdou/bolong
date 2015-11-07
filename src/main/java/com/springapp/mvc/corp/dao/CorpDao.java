e245c899a53db9f54300c93f951a244b6557d0a2
cruiserdou@qq.com <tuowei@2015>
2015-11-07 18:45:08 +0800

    sql insert@jj

--- src/main/java/com/springapp/mvc/corp/dao/CorpDao.java
+++ src/main/java/com/springapp/mvc/corp/dao/CorpDao.java
@@ -376,6 +376,125 @@ public interface CorpDao {
             "            #{corpBase.regaddr}, #{corpBase.offaddr}, #{corpBase.scope}, #{corpBase.mbus}, #{corpBase.eprofile}, #{corpBase.phoinf}, #{corpBase.remark}, #{corpBase.indclass1}, " +
             "            #{corpBase.indclass2}, #{corpBase.indclass3}, #{corpBase.indclass4}, #{corpBase.csrc_type1}, #{corpBase.csrc_type2} " +
             "            );" +
+
+            " INSERT INTO work.tb_corp_finance( " +
+            "            fin_corp_id, start_time, end_time, st_money_fund, end_money_fund,  " +
+            "            st_jyxjr_assets, end_jyxjr_assets, st_ys_bill, end_ys_bill, st_ys_account,  " +
+            "            end_ys_account, st_yf_money, end_yf_money, st_ys_interest, end_ys_interest,  " +
+            "            st_ys_dividends, end_ys_dividends, st_other_ys_money, end_other_ys_money,  " +
+            "            st_inventory, end_inventory, st_ynndq_no_assets, end_ynndq_no_assets,  " +
+            "            st_other_assets, end_other_assets, st_hj_assets, end_hj_assets,  " +
+            "            st_kgcs_assets, end_kgcs_assets, st_cyzdq_investment, end_cyzdq_investment,  " +
+            "            st_long_ys_money, end_long_ys_money, st_long_gq_investment, end_long_gq_investment,  " +
+            "            st_invest_house, end_invest_house, st_gd_assets, end_gd_assets,  " +
+            "            st_accu_deprec, end_accu_deprec, st_gd_assets_jz, end_gd_assets_jz,  " +
+            "            st_gd_assets_ready, end_gd_assets_ready, st_gd_assets_je, end_gd_assets_je,  " +
+            "            st_now_project, end_now_project, st_project_material, end_project_material,  " +
+            "            st_gd_assets_ql, end_gd_assets_ql, st_scx_investment, end_scx_investment,  " +
+            "            st_wx_assets, end_wx_assets, st_goodwill, end_goodwill, st_cqdt_cost,  " +
+            "            end_cqdt_cost, st_dysds_assets, end_dysds_assets, st_other_no_assets,  " +
+            "            end_other_no_assets, st_hj_no_asset, end_hj_no_asset, st_hj_total_asset,  " +
+            "            end_hj_total_asset, st_short_borrow, end_short_borrow, st_jyx_finance_fz,  " +
+            "            end_jyx_finance_fz, st_yf_bill, end_yf_bill, st_yf_account, end_yf_account,  " +
+            "            st_ys_money, end_ys_money, st_yf_staff_pay, end_yf_staff_pay,  " +
+            "            st_yj_tax, end_yj_tax, st_yf_interest, end_yf_interest, st_yf_dividends,  " +
+            "            end_yf_dividends, st_other_yf_money, end_other_yf_money, st_ynndq_no_fz,  " +
+            "            end_ynndq_no_fz, st_other_fz, end_other_fz, st_hj_fz, end_hj_fz,  " +
+            "            st_long_borrow, end_long_borrow, st_yf_bond, end_yf_bond, st_long_yf_money,  " +
+            "            end_long_yf_money, st_zx_yf_money, end_zx_yf_money, st_yj_fz,  " +
+            "            end_yj_fz, st_dysds_fz, end_dysds_fz, st_other_no_fz, end_other_no_fz,  " +
+            "            st_hj_no_fz, end_hj_no_fz, st_hj_total_fz, end_hj_total_fz, st_paid_assets,  " +
+            "            end_paid_assets, st_zb_reserve, end_zb_reserve, st_kc_stock,  " +
+            "            end_kc_stock, st_zx_reserve, end_zx_reserve, st_yy_reserve, end_yy_reserve,  " +
+            "            st_wfp_profit, end_wfp_profit, st_hj_owner_right, end_hj_owner_right,  " +
+            "            st_hj_fz_owner_right, end_hj_fz_owner_right) " +
+            "    VALUES (  #{corpFinance.fin_corp_id}, #{corpFinance.start_time}, #{corpFinance.end_time}, #{corpFinance.st_money_fund}, #{corpFinance.end_money_fund}, #{corpFinance. " +
+            "            st_jyxjr_assets}, #{corpFinance.end_jyxjr_assets}, #{corpFinance.st_ys_bill}, #{corpFinance.end_ys_bill}, #{corpFinance.st_ys_account}, #{corpFinance. " +
+            "            end_ys_account}, #{corpFinance.st_yf_money}, #{corpFinance.end_yf_money}, #{corpFinance.st_ys_interest}, #{corpFinance.end_ys_interest}, #{corpFinance. " +
+            "            st_ys_dividends}, #{corpFinance.end_ys_dividends}, #{corpFinance.st_other_ys_money}, #{corpFinance.end_other_ys_money}, #{corpFinance. " +
+            "            st_inventory}, #{corpFinance.end_inventory}, #{corpFinance.st_ynndq_no_assets}, #{corpFinance.end_ynndq_no_assets}, #{corpFinance. " +
+            "            st_other_assets}, #{corpFinance.end_other_assets}, #{corpFinance.st_hj_assets}, #{corpFinance.end_hj_assets}, #{corpFinance. " +
+            "            st_kgcs_assets}, #{corpFinance.end_kgcs_assets}, #{corpFinance.st_cyzdq_investment}, #{corpFinance.end_cyzdq_investment}, #{corpFinance. " +
+            "            st_long_ys_money}, #{corpFinance.end_long_ys_money}, #{corpFinance.st_long_gq_investment}, #{corpFinance.end_long_gq_investment}, #{corpFinance. " +
+            "            st_invest_house}, #{corpFinance.end_invest_house}, #{corpFinance.st_gd_assets}, #{corpFinance.end_gd_assets}, #{corpFinance. " +
+            "            st_accu_deprec}, #{corpFinance.end_accu_deprec}, #{corpFinance.st_gd_assets_jz}, #{corpFinance.end_gd_assets_jz}, #{corpFinance. " +
+            "            st_gd_assets_ready}, #{corpFinance.end_gd_assets_ready}, #{corpFinance.st_gd_assets_je}, #{corpFinance.end_gd_assets_je}, #{corpFinance. " +
+            "            st_now_project}, #{corpFinance.end_now_project}, #{corpFinance.st_project_material}, #{corpFinance.end_project_material}, #{corpFinance. " +
+            "            st_gd_assets_ql}, #{corpFinance.end_gd_assets_ql}, #{corpFinance.st_scx_investment}, #{corpFinance.end_scx_investment}, #{corpFinance. " +
+            "            st_wx_assets}, #{corpFinance.end_wx_assets}, #{corpFinance.st_goodwill}, #{corpFinance.end_goodwill}, #{corpFinance.st_cqdt_cost}, #{corpFinance. " +
+            "            end_cqdt_cost}, #{corpFinance.st_dysds_assets}, #{corpFinance.end_dysds_assets}, #{corpFinance.st_other_no_assets}, #{corpFinance. " +
+            "            end_other_no_assets}, #{corpFinance.st_hj_no_asset}, #{corpFinance.end_hj_no_asset}, #{corpFinance.st_hj_total_asset}, #{corpFinance. " +
+            "            end_hj_total_asset}, #{corpFinance.st_short_borrow}, #{corpFinance.end_short_borrow}, #{corpFinance.st_jyx_finance_fz}, #{corpFinance. " +
+            "            end_jyx_finance_fz}, #{corpFinance.st_yf_bill}, #{corpFinance.end_yf_bill}, #{corpFinance.st_yf_account}, #{corpFinance.end_yf_account}, #{corpFinance. " +
+            "            st_ys_money}, #{corpFinance.end_ys_money}, #{corpFinance.st_yf_staff_pay}, #{corpFinance.end_yf_staff_pay}, #{corpFinance. " +
+            "            st_yj_tax}, #{corpFinance.end_yj_tax}, #{corpFinance.st_yf_interest}, #{corpFinance.end_yf_interest}, #{corpFinance.st_yf_dividends}, #{corpFinance. " +
+            "            end_yf_dividends}, #{corpFinance.st_other_yf_money}, #{corpFinance.end_other_yf_money}, #{corpFinance.st_ynndq_no_fz}, #{corpFinance. " +
+            "            end_ynndq_no_fz}, #{corpFinance.st_other_fz}, #{corpFinance.end_other_fz}, #{corpFinance.st_hj_fz}, #{corpFinance.end_hj_fz}, #{corpFinance. " +
+            "            st_long_borrow}, #{corpFinance.end_long_borrow}, #{corpFinance.st_yf_bond}, #{corpFinance.end_yf_bond}, #{corpFinance.st_long_yf_money}, #{corpFinance. " +
+            "            end_long_yf_money}, #{corpFinance.st_zx_yf_money}, #{corpFinance.end_zx_yf_money}, #{corpFinance.st_yj_fz}, #{corpFinance. " +
+            "            end_yj_fz}, #{corpFinance.st_dysds_fz}, #{corpFinance.end_dysds_fz}, #{corpFinance.st_other_no_fz}, #{corpFinance.end_other_no_fz}, #{corpFinance. " +
+            "            st_hj_no_fz}, #{corpFinance.end_hj_no_fz}, #{corpFinance.st_hj_total_fz}, #{corpFinance.end_hj_total_fz}, #{corpFinance.st_paid_assets}, #{corpFinance. " +
+            "            end_paid_assets}, #{corpFinance.st_zb_reserve}, #{corpFinance.end_zb_reserve}, #{corpFinance.st_kc_stock}, #{corpFinance. " +
+            "            end_kc_stock}, #{corpFinance.st_zx_reserve}, #{corpFinance.end_zx_reserve}, #{corpFinance.st_yy_reserve}, #{corpFinance.end_yy_reserve}, #{corpFinance. " +
+            "            st_wfp_profit}, #{corpFinance.end_wfp_profit}, #{corpFinance.st_hj_owner_right}, #{corpFinance.end_hj_owner_right}, #{corpFinance. " +
+            "            st_hj_fz_owner_right}, #{corpFinance.end_hj_fz_owner_right} );"+
+            
+            " INSERT INTO work.tb_corp_contact( " +
+            "            cont_corp_id, cont_name, cont_psotion, cont_edoctype,  " +
+            "            cont_edocnum, cont_ephone, cont_efax, cont_eemail, cont_eqq,  " +
+            "            cont_webchat, cont_tel, cont_bz) " +
+            "   VALUES (#{corpContact.cont_corp_id}, #{corpContact.cont_name}, #{corpContact.cont_psotion}, #{corpContact.cont_edoctype}," +
+            "           #{corpContact.cont_edocnum}, #{corpContact.cont_ephone}, #{corpContact.cont_efax},  #{corpContact.cont_eemail}, " +
+            "           #{corpContact.cont_eqq}, #{corpContact.cont_webchat}, #{corpContact.cont_tel},  #{corpContact.cont_bz} );" +
+
+            " INSERT INTO work.tb_corp_government( " +
+            "            gov_corp_id, gov_domain, gov_office, gov_desc, gov_contact,  " +
+            "            gov_psotion, gov_doctype, gov_docnum, gov_phone, gov_fax, gov_email,  " +
+            "            gov_qq, gov_webchat, gov_tel, gov_remark) " +
+            "   VALUES (#{corpGov.gov_corp_id}, #{corpGov.gov_domain}, #{corpGov.gov_office}, #{corpGov.gov_desc},  " +
+            "           #{corpGov.gov_contact}, #{corpGov.gov_psotion}, #{corpGov.gov_doctype}, #{corpGov.gov_docnum}, " +
+            "           #{corpGov.gov_phone}, #{corpGov.gov_fax}, #{corpGov.gov_email}, #{corpGov.gov_qq},  " +
+            "           #{corpGov.gov_webchat}, #{corpGov.gov_tel}, #{corpGov.gov_remark} ); "+
+
+            " INSERT INTO work.tb_corp_investors( " +
+            "            inv_corp_id, inv_domain, inv_csrc_type1, inv_csrc_type2,  " +
+            "            inv_csrc_type3, inv_csrc_type4, inv_indclass1, inv_indclass2,  " +
+            "            inv_contact, inv_psotion, inv_doctype,  inv_docnum,  " +
+            "            inv_phone, inv_fax, inv_email, inv_qq, inv_webchat,  " +
+            "            inv_tel, inv_remark) " +
+            "   VALUES (#{corpInvestor.inv_corp_id}, #{corpInvestor.inv_domain}, #{corpInvestor.inv_csrc_type1}, #{corpInvestor.inv_csrc_type2},  " +
+            "           #{corpInvestor.inv_csrc_type3}, #{corpInvestor.inv_csrc_type4}, #{corpInvestor.inv_indclass1}, #{corpInvestor.inv_indclass2}, " +
+            "           #{corpInvestor.inv_contact}, #{corpInvestor.inv_psotion}, #{corpInvestor.inv_doctype}, #{corpInvestor.inv_docnum}, " +
+            "           #{corpInvestor.inv_phone}, #{corpInvestor.inv_fax}, #{corpInvestor.inv_email}, #{corpInvestor.inv_qq}, " +
+            "           #{corpInvestor.inv_webchat}, #{corpInvestor.inv_tel}, #{corpInvestor.inv_remark} ); "+
+
+            " INSERT INTO work.tb_corp_refinancing( " +
+            "            refi_corp_id, refi_amounts, refi_use, refi_financ, refi_security,  " +
+            "            refi_acc_cost, refi_deadline, refi_desc) " +
+            "    VALUES (#{corpReFinancing.refi_corp_id}, #{corpReFinancing.refi_amounts}, #{corpReFinancing.refi_use}, #{corpReFinancing.refi_financ},  " +
+            "             #{corpReFinancing.refi_security}, #{corpReFinancing.refi_acc_cost}, #{corpReFinancing.refi_deadline}, #{corpReFinancing.refi_desc}); "+
+
+            " INSERT INTO work.tb_corp_rehr( " +
+            "            rehr_corp_id, rehr_post, rehr_num, rehr_salary, rehr_sex_req,  " +
+            "            rehr_age_req, rehr_requests) " +
+            "   VALUES (#{corpReHr.rehr_corp_id}, #{corpReHr.rehr_post}, #{corpReHr.rehr_num}, #{corpReHr.rehr_salary},  " +
+            "           #{corpReHr.rehr_sex_req}, #{corpReHr.rehr_age_req}, #{corpReHr.rehr_requests}); "+
+
+            " INSERT INTO work.tb_corp_retrain( " +
+            "            retra_corp_id, retra_mode, retra_content, retra_acc_cost, " +
+            "            retra_dt, retra_requests) " +
+            "   VALUES (#{corpReTrain.retra_corp_id}, #{corpReTrain.retra_mode}, #{corpReTrain.retra_content}, #{corpReTrain.retra_acc_cost},  " +
+            "           #{corpReTrain.retra_dt}, #{corpReTrain.retra_requests}); "+
+
+
+            " INSERT INTO work.tb_corp_service( " +
+            "            ssrv_corp_id, srv_name, srv_type, srv_content, srv_levels, srv_domain, " +
+            "             srv_penalty, srv_examiner, srv_post, srv_descs, srv_remark) " +
+            "   VALUES (#{corpServicePojo.ssrv_corp_id}, #{corpServicePojo.srv_name}, #{corpServicePojo.srv_type}, #{corpServicePojo.srv_content},  " +
+            "           #{corpServicePojo.srv_levels}, #{corpServicePojo.srv_domain}, #{corpServicePojo.srv_penalty},  #{corpServicePojo.srv_examiner},   " +
+            "           #{corpServicePojo.srv_post}, #{corpServicePojo.srv_descs}, #{corpServicePojo.srv_remark});" +
+
+
             "end;")
     void insertCorp(
             @Param(value = "corpBase") CorpBase corpBase,
